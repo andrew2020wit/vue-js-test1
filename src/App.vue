b055933689ef52b9958e25cf78a990b9740347e7
andrew2020wit <andrew2020wit@gmail.com>
2020-12-23 18:37:15 +0200

    init2

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,70 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="body-div">
+    <div class="layout">
+      <header>VueJS Test1</header>
+      <main>
+        <nav>
+          <!-- Menu -->
+          <ul>
+            <li>
+              <router-link to="/"> Home</router-link>
+            </li>
+            <li>
+              <router-link to="/about">About</router-link>
+            </li>
+          </ul>
+
+          <br />
+        </nav>
+        <div class="content">
+          <!-- Content -->
+          <router-view />
+        </div>
+      </main>
+      <footer>Footer</footer>
+    </div>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+ul li {
+  margin: 0.5rem;
+  font-size: 1.5rem;
+}
+.body-div {
+  margin: 0;
+  padding: 0;
+}
+.layout {
+  display: flex;
+  flex-direction: column;
+  background-color: red;
+  flex-wrap: nowrap;
+  height: 100vh;
+}
+header {
+  background-color: aqua;
+  flex: 0;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+main {
+  background-color: aquamarine;
+  flex: 1;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+}
+footer {
+  background-color: aqua;
+  flex: 0;
+}
+nav {
+  width: fit-content;
+  flex: 0;
+  background-color: aquamarine;
+}
+.content {
+  flex: 1;
+  background-color: burlywood;
 }
 </style>
